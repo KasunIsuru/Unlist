c3cfcfc1241f1d48fefb44f5075117519e1c2210
kasun Isuru <72108374+KasunIsuru@users.noreply.github.com>
2024-06-14 17:36:44 +0530

    Calender fragment is done

--- app/src/main/java/com/example/unlist/CalendarFragment.java
+++ app/src/main/java/com/example/unlist/CalendarFragment.java
@@ -1,19 +1,25 @@
 package com.example.unlist;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.CalendarView;
 
 public class CalendarFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        CalendarView calendarView = view.findViewById(R.id.calendarView);
+
+
+        long today = System.currentTimeMillis();
+        calendarView.setDate(today, true, true);
+
+        return view;
     }
 }
